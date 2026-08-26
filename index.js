bef35b10f4322de75db99efe74127e850b3112b9
moe <moe@indicium.ag>
2026-08-26 10:17:03 +0200

    make the buttons return values. Calculator value prints to the console. Only numbers are  printed. Dot is added now once and only once.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-console.log("test");
-const container = document.querySelectorAll(".key");
-console.dir(container);
+console.log("The file is successfully connected");
+
+const buttonList = document.querySelectorAll(".key");
+console.dir(buttonList);
+
+let calculatorValue = "0";
+
+const calculatorClickHandler = (evt) => {
+  const alreadyHasADot = calculatorValue.includes(".");
+  const isDot = evt.target.innerText.includes(".");
+  const shouldSkip = alreadyHasADot && isDot;
+  if (!shouldSkip) {
+    calculatorValue += evt.target.innerText;
+  }
+
+  console.log({ calculatorValue });
+};
+
+for (const element of buttonList) {
+  const hasNumber = element.className.includes("number");
+  if (hasNumber) element.addEventListener("click", calculatorClickHandler);
+}
